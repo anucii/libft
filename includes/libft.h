03c336fa45f9e98d42d8326e392db7e008fead7e
Jean-Judicael DAUFIN <jdaufin@student.42.fr>
2017-09-30 19:54:47 +0200

    List link deletion function added

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jdaufin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 16:19:39 by jdaufin           #+#    #+#             */
-/*   Updated: 2017/09/29 21:12:21 by jdaufin          ###   ########.fr       */
+/*   Updated: 2017/09/30 19:52:59 by jdaufin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,6 +136,7 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstappend(t_list **alst, t_list *new);
 void				ft_lstinsert(t_list **addr, t_list *new);
+void				ft_linkdel(void *ptr, size_t size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
