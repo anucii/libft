fa2221129972f87d1f22ea5031a1985727dec9a1
Jean-Judicael DAUFIN <jdaufin@student.42.fr>
2017-11-04 13:59:32 +0100

    ft_lstpop : attempt to handle properly monolink lists

--- srcs/ft_lstpop.c
+++ srcs/ft_lstpop.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/05 14:31:23 by jdaufin           #+#    #+#             */
-/*   Updated: 2017/11/03 18:28:07 by jdaufin          ###   ########.fr       */
+/*   Updated: 2017/11/04 13:58:22 by jdaufin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	ft_lstpop(t_list **alst)
 {
 	t_list	*buf[2];
+	_Bool	uniq;
 
-	if (!(alst && *alst && (*alst)->next))
+	if (!(alst && *alst))
 		return ;
 	buf[0] = *alst;
+	uniq = (*alst)->next ? 0 : 1;
 	while (buf[0]->next)
 	{
 		buf[1] = buf[0];
 		buf[0] = buf[0]->next;
 	}
 	ft_lstdelone(&buf[0], &ft_linkdel);
-	buf[1]->next = NULL;
+	if (!uniq)
+		buf[1]->next = NULL;
 }
