a6ce3b7500e1fa6f745a9650f945ed60e3310d0c
jdaufin <jdaufin@student.42.fr>
2021-05-16 16:39:23 +0200

    ft_printf : removal of tags mismatches tests, due to va_arg inherent limitations even unhandled by the real printf

--- demo.c
+++ demo.c
@@ -25,24 +25,16 @@ int main(int ac, char **av)
 	ft_printf("ft_printf oddities [apparent but handled mismatches]\n\t- hex(Oxa1) for base10 int: %d\n", 0xa1);
 	ft_printf_fd(fd, "ft_printf_fd oddities [apparent but handled mismatches]\n\t- hex(Oxa1) for base10 int: %d\n", 0xa1);
 
-
-
 	ft_printf("\n\033[34mft_printf crash tests\033[0m\n");
 	ft_printf("ft_printf crash test [more tags than arguments]\n\t%s - %s\n", "One arg only");
 	ft_printf("ft_printf crash test [more arguments than tags]\n\t%s\n", "First argument", "Ignored argument");
 	ft_printf("ft_printf crash test [arguments but no tags]\n", "Undue argument");
-	ft_printf("ft_printf crash test [arguments wrong types]\n\t- int for string: %s\n", -1);
-	ft_printf("ft_printf crash test [arguments wrong types]\n\t- string for int: %d\n", "not really an int");
-	ft_printf("ft_printf crash test [arguments wrong types]\n\t- string for char: %c\n", "too long");
-	ft_printf("ft_printf crash test [arguments wrong types]\n\t- float for int: %d\n", 1.5);
-
+	
 	ft_printf("\n\033[34mft_printf_fd crash tests\033[0m\n");
-	ft_printf_fd(fd, "ft_printf_fd crash test [arguments wrong types]\n\t- float for int: %d\n", 1.5);	ft_printf_fd(fd, "ft_printf_fd crash test [more tags than arguments]\n\t%s - %s\n", "One arg only");
+	ft_printf_fd(fd, "ft_printf_fd crash test [arguments wrong types]\n\t- float for int: %d\n", 1.5);	
+	ft_printf_fd(fd, "ft_printf_fd crash test [more tags than arguments]\n\t%s - %s\n", "One arg only");
 	ft_printf_fd(fd, "ft_printf_fd crash test [more arguments than tags]\n\t%s\n", "First argument", "Ignored argument");
 	ft_printf_fd(fd, "ft_printf_fd crash test [arguments but no tags]\n", "Undue argument");
-	ft_printf_fd(fd, "ft_printf_fd crash test [arguments wrong types]\n\t- int for string: %s\n", -1);
-	ft_printf_fd(fd, "ft_printf_fd crash test [arguments wrong types]\n\t- string for int: %d\n", "not really an int");
-	ft_printf_fd(fd, "ft_printf_fd crash test [arguments wrong types]\n\t- string for char: %c\n", "too long");
-	
+
 	return 0;
 }
